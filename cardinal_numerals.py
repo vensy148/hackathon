80a97ea4225078ba247f77934b6a61b6ca0a3882
Pham Hoang Tien <phamuyt@gmail.com>
2019-07-15 15:38:48 +0700

    commit code for waypoint 3

--- cardinal_numerals.py
+++ cardinal_numerals.py
@@ -1,5 +1,5 @@
-# import pygame
-# pygame.init()
+import pygame
+pygame.init()
 
 def integer_to_vietnamese_numeral(s, region='north', activate_tts = False):
 
@@ -13,6 +13,42 @@ def integer_to_vietnamese_numeral(s, region='north', activate_tts = False):
         print(s)
     else: print(s)
 
+    # Waypoint 3: Say Vietnamese Numeral
+    file_name_map = {
+        "một" : "mot1",
+        "hai" : "hai",
+        "ba" : "ba",
+        "bốn" : "bon",
+        "năm" : "nam",
+        "sáu" : "sau",
+        "bảy" : "bay",
+        "tám" : "tam",
+        "chín" : "chin",
+        "không" : "khong",
+        "mốt" : "mot2",
+        "lăm" : "lam",
+        "mươi" : "muoi1",
+        "mười" : "muoi2",
+        "ngàn" : "ngan",
+        "nghìn" : "nghin",
+        "trăm" : "tram",
+        "triệu" : "trieu",
+        "tỷ" : "ty",
+        "lẻ" : "le",
+        "linh" : "linh"
+    }
 
+    if region == "north":
+        if activate_tts == None: activate_tts = False
+        if not isinstance(activate_tts, bool): raise TypeError("Argument activate_tts is not a boolean")
+        if activate_tts == True:
+            for word in s.split(" "):
+                file_location = "./sounds/vie/north/" + file_name_map[word] + ".ogg"
+                print(file_location)
+                pygame.mixer.Sound(file_location).play(maxtime = 1000)
+                pygame.time.delay(600)
+
+# ss = "bốn trăm linh năm"
+# integer_to_vietnamese_numeral(ss,'south', True)
 # integer_to_vietnamese_numeral(ss, region='')
 # print(file_name_map("bốn"))
